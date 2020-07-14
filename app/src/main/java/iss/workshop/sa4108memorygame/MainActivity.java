70e0687b87610fae291412556cf2dd48c92061c5
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-07-14 21:40:21 +0800

    Added new function to have gridview display 4 columns instead of 3.

--- app/src/main/java/iss/workshop/sa4108memorygame/MainActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
@@ -13,7 +14,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     //testdata
     private String[] cartoons = {
@@ -47,11 +48,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ImageAdapter imgAdapter =new ImageAdapter(this,R.layout.image_row, (ArrayList<String>) this.testlist1);
                 GridView gridView1 = findViewById(R.id.gridView1);
                 gridView1.setEnabled(false);
+                gridView1.setNumColumns(4);
                 if (gridView1 != null){
                     gridView1.setAdapter(imgAdapter);
+                    gridView1.setOnItemClickListener(this);
+
                 }
                 break;
 
+        }
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        switch(adapterView.getId()){
+            //This is still not working well - need to resolve
+            case R.id.img1:
+                System.out.println("This is onItemClick");
         }
 
     }
