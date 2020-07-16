d91345f60ef856fde76b0866b9ca80314cf6a4a5
Rohan <63039249+e0533412@users.noreply.github.com>
2020-07-16 15:38:23 +0800

    - added intent to move user from Result to ScoreBoard

--- app/src/main/java/iss/workshop/sa4108memorygame/ResultActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/ResultActivity.java
@@ -2,15 +2,34 @@ package iss.workshop.sa4108memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private Button scoreboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
+        scoreboard = (Button) findViewById(R.id.scoreboard);
+        scoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScoreboard();
+            }
+        });
+
+    }
+
+    public void openScoreboard() {
+        Intent intent = new Intent(this, ScoreActivity.class);
 
     }
 }
