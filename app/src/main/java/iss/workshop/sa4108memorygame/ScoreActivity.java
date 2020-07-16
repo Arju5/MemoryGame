dc337de8274c563da0bc102adc1d945e62b02afe
Rohan <63039249+e0533412@users.noreply.github.com>
2020-07-16 15:15:06 +0800

    - commented out extra buttons for main activity_main.xml - removed buttons can be shown as fragment, or on activity_result.xml as additional actions post-game

--- app/src/main/java/iss/workshop/sa4108memorygame/ScoreActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/ScoreActivity.java
@@ -3,6 +3,7 @@ package iss.workshop.sa4108memorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+
+        TextView playerName = (TextView) findViewById(R.id.playerName);
+        TextView playerScore = (TextView) findViewById(R.id.playerScore);
+
+        int score = 0;
     }
 }
