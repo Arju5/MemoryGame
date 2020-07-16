be79bedb6ce9270778fd2c1258493cf46c2a1a37
Arju5 <arjun.chirumalle@gmail.com>
2020-07-16 20:10:33 +0800

    - dynamically add table data for score board

--- app/src/main/java/iss/workshop/sa4108memorygame/ScoreActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/ScoreActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    TextView rank, name, time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +22,29 @@ public class ScoreActivity extends AppCompatActivity {
 
 
         /*TextView playerName = (TextView) findViewById(R.id.playerName);
-        TextView playerScore = (TextView) findViewById(R.id.playerScore);*/
+        TextView playerScore = (TextView) findViewById(R.id.playerScore);
+        float score = intent.getFloatExtra("score", 10.0f);*/
+        TableLayout table = (TableLayout) findViewById(R.id.table);
+        for(int i = 1; i < 6 ; i ++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setShowDividers(LinearLayout.SHOW_DIVIDER_END);
+            rank = new TextView(this);
+            rank.setText("4");
+            rank.setTextSize(20);
+            rank.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            name = new TextView(this);
+            name.setText("Team 3");
+            name.setTextSize(20);
+            name.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            time = new TextView(this);
+            time.setText("Score");
+            time.setTextSize(20);
+            time.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            tableRow.addView(rank);
+            tableRow.addView(name);
+            tableRow.addView(time);
+            table.addView(tableRow);
+        }
 
-
-
-        float score = intent.getFloatExtra("score", 10.0f);
     }
 }
