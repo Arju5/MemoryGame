19a6da4b93cf1ccff98cd0fcfaad390ee42969b0
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-07-16 09:57:12 +0800

    Added Progress Bar at the bottom

--- app/src/main/java/iss/workshop/sa4108memorygame/URLParserThread.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/URLParserThread.java
@@ -1,9 +1,15 @@
 package iss.workshop.sa4108memorygame;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+
 import java.io.IOException;
 
 public class URLParserThread extends Thread{
     private String url;
+
 
     public URLParserThread(String url){
         super();
@@ -12,14 +18,21 @@ public class URLParserThread extends Thread{
 
     @Override
     public void run() {
+
+
         HTMLParser htmlParser = new HTMLParser(url);
         try {
             String htmlString = htmlParser.CreateHTMLString();
             System.out.println(htmlString);
+
+            Looper mainThreadLooper = Looper.getMainLooper(); // --> Looper of the main/UI thread
+            Handler mainThreadHandler = new Handler(mainThreadLooper); // --> Get handler to main thread
+            Message messageToSendToMainThread = Message.obtain(); // --> Create a message to send to UI thread
+            messageToSendToMainThread.obj = htmlString; // htmlString -> actual msg value
+            mainThreadHandler.sendMessage(messageToSendToMainThread);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
