0d2dca9d08a3a94e31915997cf7ff00415bb69c7
ShweYM <38621520+ShweYM@users.noreply.github.com>
2020-07-17 21:22:20 +0800

    test_bgsong and shuffle

--- app/src/main/java/iss/workshop/sa4108memorygame/DetailActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/DetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,25 +18,16 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class DetailActivity extends AppCompatActivity {
-
-    ImageView currentView = null;
-    TextView timer_txt;
-    private int countPair = 0;
-    boolean timer_start = true;
-    long start_time = 0;
-    private boolean isInGame = true;
-    private String firstImg;
-    private String secondImg;
-    private int[] randomImage;
-    private int numOfImages = 12;
-    //private int[] imageGraphic = new int[numOfImages/2];
-    private int[] imageGraphicLocation = new int[numOfImages];
 
     class DetailTask extends TimerTask {
 
@@ -57,14 +49,12 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    ;
-
-    final int[] selectedImg = new int[]{
-            R.drawable.hug, R.drawable.laugh, R.drawable.peep,
-            R.drawable.snore, R.drawable.stop, R.drawable.tired,
-            R.drawable.hug, R.drawable.laugh, R.drawable.peep,
-            R.drawable.snore, R.drawable.stop, R.drawable.tired
-    };
+    Timer timer;
+    ImageView currentView = null;
+    TextView timer_txt;
+    private int countPair = 0;
+    boolean timer_start = true;
+    long start_time = 0;
 
     int firstClick = 0;
     int secondClick = 0;
@@ -77,77 +67,133 @@ public class DetailActivity extends AppCompatActivity {
     boolean isMatched = false;
     String countMsg;
 
-//    final String[] selectedImg = new String[]{
-//         "hug", "laugh", "peep", "snore", "stop","tired"};
+    //final HashMap<Integer,Integer> dict = new HashMap<Integer, Integer>();
+    List<Integer> ll = new ArrayList<>();
 
-    int[] pos = {0,1,2,3,4,5,0,1,2,3,4,5};
+    final int[] selectedImg = new int[]{
+            R.drawable.hug, R.drawable.laugh, R.drawable.peep,
+            R.drawable.snore, R.drawable.stop, R.drawable.tired,
+    };
+
+    int[] pos = {0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5};
     int currentPos = -1;
 
-    Timer timer;
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        //Timer start when arrives this activity
         timer_txt = (TextView) findViewById(R.id.timer);
         if (timer_start) {
             start_time = System.currentTimeMillis();
             timer = new Timer();
             timer.schedule(new DetailTask(), 0, 500);
-            //h2.postDelayed(run, 0);
         } else {
 
         }
+
+        //for background music
+        player = MediaPlayer.create(this,R.raw.over_the_rainbow);
+        player.start();
+        player.setLooping(true);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true)
+                    System.out.println("Running...");
+            }
+        }).start();
+        //////////////////////////
+
+        /*dict.put(0,R.drawable.hug);
+        dict.put(1,R.drawable.laugh);
+        dict.put(2,R.drawable.peep);
+        dict.put(3,R.drawable.snore);
+        dict.put(4,R.drawable.stop);
+        dict.put(5,R.drawable.tired);
+        dict.put(6,R.drawable.hug);
+        dict.put(7,R.drawable.laugh);
+        dict.put(8,R.drawable.peep);
+        dict.put(9,R.drawable.snore);
+        dict.put(10,R.drawable.stop);
+        dict.put(11,R.drawable.tired);*/
+
+        for(int i = 0 ; i < 2;i++){
+            for(int j = 0;j<selectedImg.length;j++){
+                ll.add(selectedImg[j]);
+            }
+        }
+
+        /*ll.add(R.drawable.hug);
+        ll.add(R.drawable.laugh);
+        ll.add(R.drawable.peep);
+        ll.add(R.drawable.snore);
+        ll.add(R.drawable.stop);
+        ll.add(R.drawable.tired);
+        ll.add(R.drawable.hug);
+        ll.add(R.drawable.laugh);
+        ll.add(R.drawable.peep);
+        ll.add(R.drawable.snore);
+        ll.add(R.drawable.stop);
+        ll.add(R.drawable.tired);*/
+
+        for(int i=0;i<ll.size();i++){
+            System.out.println("List : " + ll.get(i));
+        }
+
+        System.out.println("FirstTime : " + ll.get(0));
+        Collections.shuffle(ll);
+        System.out.println("SecondTime : " + ll.get(0));
+
         GridView gridView = (GridView) findViewById(R.id.gridView2);
         ImageAdapter2 adapter = new ImageAdapter2(this);
-        //shuffleImage();
+
         if (gridView != null) {
             gridView.setAdapter(adapter);
             gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                    if(((ImageView)view).isEnabled() == true){
-                        if(isBusy){
+                    if (((ImageView) view).isEnabled() == true) {
+                        if (isBusy) {
                             return;
                         }
-                        if(isMatched){
+                        if (isMatched) {
                             return;
                         }
-                        if(firstview == null){
-                            firstClick = selectedImg[pos[i]];
+                        if (firstview == null) {
+                            firstClick = ll.get(i);
 
-                            firstview = (ImageView)view;
-                            //firstview.setEnabled(false);
-                            flip(firstview,i,"first");
+                            firstview = (ImageView) view;
+                            flip(firstview, i, "first");
 
                             return;
                         }
-                    /*if(firstview.getId() == view.getId()){
-                        return;
-                    }*/
+
                         System.out.println("FirstViewID : " + firstClick);
-                        System.out.println("SecondViewID : " + selectedImg[pos[i]]);
+                        System.out.println("SecondViewID : " + ll.get(i));
 
-                        if(firstClick != selectedImg[pos[i]]){
+                        if (firstClick != ll.get(i)) {
                             secondview = (ImageView) view;
-                            flip(secondview,i,"second");
+                            flip(secondview, i, "second");
                             isBusy = true;
 
                             final Handler handler = new Handler();
                             handler.postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
-                                    flip(secondview,0,"second");
-                                    flip(firstview,0,"first");
+                                    flip(secondview, 0, "second");
+                                    flip(firstview, 0, "first");
                                     firstview = null;
                                     secondview = null;
                                     isBusy = false;
                                 }
-                            },1000);
-                        }
-                        else {
+                            }, 1000);
+                        } else {
                             secondview = (ImageView) view;
                             flip(secondview, i, "second");
 
@@ -156,22 +202,22 @@ public class DetailActivity extends AppCompatActivity {
                             //increasing count of matches
                             //isMatched = true;
                             countPair++;
-                            countMsg = countPair+"/6 Matches";
+                            countMsg = countPair + "/6 Matches";
                             TextView count = findViewById(R.id.NoOfMatches);
-                            if(count!=null) {
+                            if (count != null) {
                                 count.setText(countMsg);
                             }
-                            if(countPair == 6){
+                            if (countPair == 6) {
                                 isMatched = true;
 
                                 timer.cancel();
                                 timer.purge();
 
                                 //b.setText("start");
-                                Intent intent = new Intent(DetailActivity.this,MainActivity.class);
+                                Intent intent = new Intent(DetailActivity.this, MainActivity.class);
                                 startActivity(intent);
 
-                                Toast.makeText(getApplicationContext(),"Congratulations!!! You win.",Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), "Congratulations!!! You win.", Toast.LENGTH_LONG).show();
                             }
 
                             firstview = null;
@@ -187,45 +233,54 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    public void flip(ImageView view,int id, String level){
-        if(isMatched){
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        player.seekTo(0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }
+
+
+    public void flip(ImageView view, int id, String level) {
+        if (isMatched) {
             return;
         }
-        if(level == "first"){
-            if(isFlipped1){
+        if (level == "first") {
+            if (isFlipped1) {
                 view.setImageResource(R.drawable.question);
                 isFlipped1 = false;
-            }
-            else{
-                view.setImageResource(selectedImg[pos[id]]);
+            } else {
+                view.setImageResource(ll.get(id));
                 isFlipped1 = true;
             }
         }
-        if(level == "second"){
-            if(isFlipped2){
+        if (level == "second") {
+            if (isFlipped2) {
                 view.setImageResource(R.drawable.question);
                 isFlipped2 = false;
-            }
-            else{
-                view.setImageResource(selectedImg[pos[id]]);
+            } else {
+                view.setImageResource(ll.get(id));
                 isFlipped2 = true;
             }
         }
 
-    }
-
-    protected void shuffleImage() {
-        Random rand = new Random();
-
-        for (int i = 0; i < numOfImages; i++) {
-            this.imageGraphicLocation[i] = i % (numOfImages / 2);
-        }
-        for (int i = 0; i < numOfImages; i++) {//swap location
-            int temp = this.imageGraphicLocation[i];
-            int swapIndex = rand.nextInt(16);
-            imageGraphicLocation[i] = imageGraphicLocation[swapIndex];
-            imageGraphicLocation[swapIndex] = temp;
-        }
     }
 
 }
