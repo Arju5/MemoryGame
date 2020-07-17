8f6bb2ef50c2dbabde8acd0523be05a15aadd718
Rohan <63039249+e0533412@users.noreply.github.com>
2020-07-17 19:10:08 +0800

    - getIntent to obtain score/time from DetailActivity - write to file method for submit score

--- app/src/main/java/iss/workshop/sa4108memorygame/ResultActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/ResultActivity.java
@@ -1,12 +1,21 @@
 package iss.workshop.sa4108memorygame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -14,12 +23,21 @@ public class ResultActivity extends AppCompatActivity {
     private Button scoreboard;
     private Button submit;
     private String name;
-    private int playerScore;
+    private int score;
+
+    EditText nameInput;
+    TextView playerScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+       /* Intent intent = getIntent();
+        int time = intent.getIntExtra("time");*/
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        playerScore = (TextView) findViewById(R.id.playerScore);
 
         submit = (Button) findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
@@ -61,7 +79,29 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void submitScore() {
+
+        String filePath = "ScoreBoard";
+        String fileName = "ScoreBoard.txt";
+        File mTargetFile = new File(this.getFilesDir(), filePath + "/" +fileName);
+        String fileContent = "User name1" + "," + "time1";
+
+        try{
+
+            File parent = mTargetFile.getParentFile();
+
+            if(!parent.exists() && !parent.mkdirs()) {
+                throw new IllegalStateException("Couldn't create directory: " + parent);
+            }
+            FileOutputStream fos = new FileOutputStream(mTargetFile);
+            fos.write(fileContent.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Intent mainIntent = new Intent(ResultActivity.this, ScoreActivity.class);
+
         startActivity(mainIntent);
     }
 
