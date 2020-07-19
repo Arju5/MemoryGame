6c22812073235817ffca3f14959da57dfab608ba
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-07-19 12:02:08 +0800

    Resolved 1 issue with textview after completing download

--- app/src/main/java/iss/workshop/sa4108memorygame/MainActivity.java
+++ app/src/main/java/iss/workshop/sa4108memorygame/MainActivity.java
@@ -96,8 +96,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setProgressBarVisible(false);
                 mProgressBar.setVisibility(View.GONE);
 
-                mDownloadText.setVisibility(View.VISIBLE);
-                //mDownloadText.setText("You have selected 6 pictures");
+//                mDownloadText.setVisibility(View.VISIBLE);
+                mDownloadText.setText("");
 
                 Toast.makeText(MainActivity.this,
                         "I am done downloading!", Toast.LENGTH_SHORT).show();
@@ -114,12 +114,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     };
-
-    //testdata
-//    private String[] cartoons = {
-//            "hug", "laugh", "peep", "snore", "stop",
-//            "tired", "full", "what", "afraid", "no_way"
-//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,16 +140,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (mButtonFetch !=null){
             mButtonFetch.setOnClickListener(this);
         }
-
         for(int i = 1;i<21; i++){
             String filePath = "GamePhoto";
             String fileName = "photo_" + i + ".jpg";
             File mTargetFile = new File(MainActivity.this.getFilesDir(),filePath + "/" + fileName);
             testlist1.add(mTargetFile.getAbsolutePath());
         }
-
         deleteFilesinGamePhoto(MainActivity.this);
-
     }
 
     @Override
@@ -171,26 +162,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Thread thread = new URLParserThread(urlString,MainActivity.this, mainThreadHandler);
                     thread.start();
                     isNewCount = !isNewCount;
-
                 }
                 else
                 {
                     Intent intent = new Intent(this,MainActivity.class);
                     startActivity(intent);
                 }
-
-
                 break;
-
         }
-
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
-        // checking index
-//        File dir = new File(getStringPictureList().get(0));
-//        System.out.println(dir.getParentFile());
 
         System.out.println("Index: " + String.valueOf(index));
         System.out.println("L: " + String.valueOf(l));
@@ -216,7 +199,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             this.selectedPictureArray.add(array[index]);
             new SoundPoolPlayer(this).playSoundWithRedId(R.raw.click);
             mDownloadText.setText("You have selected "+ String.valueOf(counter) + (counter==1? " picture":" pictures"));
-
         }
 
         if (counter == 6){
@@ -231,20 +213,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.removeExtra("pictureList");
         }
     }
-
-//    @Override
-//    protected void onDestroy(){
-//        super.onDestroy();
-//        File dir = new File(getStringPictureList().get(0));
-//        System.out.println(dir.getParentFile().getName());
-////        if (dir.isDirectory()) {
-////            String[] gameimgs = dir.list();
-////            System.out.println(dir.list());
-////            for (int i = 0; i < gameimgs.length; i++) {
-////                new File(dir, gameimgs[i]).delete();
-////            }
-////        }
-//    }
 
     protected void deleteFilesinGamePhoto(Context context) {
 //        String filePath = "/data/user/0/iss.workshop.sa4108memorygame/files/GamePhoto";
